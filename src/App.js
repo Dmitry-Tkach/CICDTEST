7d50556343a5c13f2209b4fec1547b2b4cc4c911
Dmitry Tkach <dmitrytkach@Dmitrys-MacBook-Pro.local>
2023-05-11 00:16:37 +0300

    Add dev branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CICD
+          Learn React CICD DEV
         </a>
       </header>
     </div>
