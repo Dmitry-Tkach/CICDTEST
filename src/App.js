072ad3eca9167d1b0c9eadceb7ffd21f6078f6d2
Dmitry Tkach <dmitrytkach@Dmitrys-MacBook-Pro.local>
2023-05-12 17:50:19 +0300

    Change hom page title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Learn React CICD Feature 2
+        Learn React CICD DELIVERY TEST DONE!
         </a>
       </header>
     </div>
