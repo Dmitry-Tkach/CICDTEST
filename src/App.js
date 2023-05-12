4791b95491ea82c8040869e9c8580c4f3782a64f
Dmitry-Tkach <99206035+Dmitry-Tkach@users.noreply.github.com>
2023-05-12 15:21:39 +0300

    Add feature (#4)
    * test feature
    
    ---------
    
    Co-authored-by: Dmitry Tkach <dmitrytkach@Dmitrys-MacBook-Pro.local>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React CICD DEV
+          Learn React CICD Feature
         </a>
       </header>
     </div>
